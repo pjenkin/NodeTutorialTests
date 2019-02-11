994aaef693beddf587e8ca6d48a5e96e3a5a7d74
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-11 10:19:47 +0000

    asserting spy called with specific arguments

--- spies/app.test.js
+++ spies/app.test.js
@@ -5,7 +5,9 @@ describe('App', () =>
   it('should call the spy correctly', () =>
   {
     var spy = expect.createSpy();
-    spy();  // 'inject' spy
-    expect(spy).toHaveBeenCalled();
+    // spy();  // 'inject' spy
+    spy('PNJ',45);  // 'inject' spy
+    // expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('PNJ',45);
   });
 });
