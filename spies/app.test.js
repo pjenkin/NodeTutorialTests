cd16cbd41656f2606ad33e25d08766f6d5d68d94
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-11 10:36:01 +0000

    rewire yn le require, pass in spy instead of genuine db 6-58

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,7 +1,17 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');    // app.__set__  app.__get__
 
 describe('App', () =>
 {
+  var db =
+  {
+      saveUser: expect.createSpy()
+  };
+  app.__set__('db',db);
+
+
   it('should call the spy correctly', () =>
   {
     var spy = expect.createSpy();
@@ -9,5 +19,17 @@ describe('App', () =>
     spy('PNJ',45);  // 'inject' spy
     // expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledWith('PNJ',45);
+  });
+
+
+  it('should call saveUser with specific user object', () =>
+  {
+    var email = 'pj@example.com';
+    var password = '123abc';
+
+    app.handleSignup(email,password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password});
+    // in ES6, same as expect(db.saveUser).toHaveBeenCalledWith({email: email, password: password});
+    // expect(db.saveUser).toHaveBeenCalledWith({email, password, something:'bogus'});
   });
 });
