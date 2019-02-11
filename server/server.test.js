6b879b23c382a9d5e101b47293ec9f4409558b83
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-11 08:23:23 +0000

    'Server' test describe block, with '#Root' and 'Users' sub-blocks. challenge 6-57

--- server/server.test.js
+++ server/server.test.js
@@ -3,48 +3,66 @@ const expect = require('expect');
 
 var app = require('./server').app;      // NB importing both module and specific method
 
-it ('should return hello mum! response', (done) =>
-{
-  request(app)
-    .get('/')   // testing root url
-    // .expect(200)      // status code 200 'OK' expected
-    .expect(404)      // status code 200 'OK' expected
-    // .expect('hello mum!')
-    // .expect('helloooooo mum!')
-    // .expect(
-    //   {
-    //     errorMessage: 'Page ain\'t found.'
-    //   }
-    .expect( (response) =>
-      {
-        // custom assertion using expect
-        expect((response.body)).toInclude(    // NB careful with brackets
-          {
-            // custom assertion wrt HTTP response body
-            // name: 'PNJ tutorial app 1.0',    // NB leave out checking for name for the mo
-            errorMessage: 'Page ain\'t found.'
-          });
-      })
-    // )
-    .end(done);     // done handled by supertest - wrapping up
-});
+// Server root describe block
+// (1) root (2) users #
 
-
-// test to assert (1) status code is 200 (2) my own user object exists (toInclude)
-it ('should contain P Jenkin name & age in a 200 response', (done) =>
+describe('Server', () =>
 {
-    request(app)
-    .get('/users')
-    .expect(200)    // condition (1) - check for status 200
-    // .expect(300)    // condition (1) - check for status 200
-    .expect((response) =>
+  describe('#Root', () =>
+  {
+    it ('should return hello mum! response', (done) =>
     {
-      expect((response.body)).toInclude(
+      request(app)
+        .get('/')   // testing root url
+        // .expect(200)      // status code 200 'OK' expected
+        .expect(404)      // status code 200 'OK' expected
+        // .expect('hello mum!')
+        // .expect('helloooooo mum!')
+        // .expect(
+        //   {
+        //     errorMessage: 'Page ain\'t found.'
+        //   }
+        .expect( (response) =>
+          {
+            // custom assertion using expect
+            expect((response.body)).toInclude(    // NB careful with brackets
+              {
+                // custom assertion wrt HTTP response body
+                // name: 'PNJ tutorial app 1.0',    // NB leave out checking for name for the mo
+                errorMessage: 'Page ain\'t found.'
+              });
+          })
+        // )
+        .end(done);     // done handled by supertest - wrapping up
+    });
+
+  });   // end of 'Root' test describe block
+
+  describe('#Users', () =>
+  {
+
+    // test to assert (1) status code is 200 (2) my own user object exists (toInclude)
+    it ('should contain P Jenkin name & age in a 200 response', (done) =>
+    {
+        request(app)
+        .get('/users')
+        .expect(200)    // condition (1) - check for status 200
+        // .expect(300)    // condition (1) - check for status 200
+        .expect((response) =>
         {
-          // name: 'P Jenkins',
-          name: 'P Jenkin',
-          age: '45'
-        });
-    })
-    .end(done);   // and wrap up this chain of expects from the HTTP response
+          expect((response.body)).toInclude(
+            {
+              // name: 'P Jenkins',
+              name: 'P Jenkin',
+              age: '45'
+            });
+        })
+        .end(done);   // and wrap up this chain of expects from the HTTP response
+    });   // end of '#users' test block
+
+
+  });   // end of 'Server' test describe block
+
+
+
 });
